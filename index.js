2688efbdac9413507c28f6af53c8194ce8e5e83e
dantedxwolf <teacher.martin@gmail.com>
2021-01-20 15:17:48 +0600

    mongoDB setup

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('This is working');
+require('dotenv').config();
+const express = require('express');
+const mongoose = require("mongoose");
+const app = express();
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+    .then((result) =>{
+        app.listen(4000,() =>{
+            console.log('mongoose connected');
+            console.log('Listening to port 4000');
+        })
+    })
+    .catch(err => {
+        console.log('Error');
+    });
